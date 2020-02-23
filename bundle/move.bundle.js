6532271cb05bf66942c92f21c32bc6572080ae17
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-02-23 16:57:28 +0100

    Create move.bundle.js

---
+++ bundle/move.bundle.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const moveing=[
+{"thiss": __dirname+"/dist/ws.bundle.js","target":__dirname+"/../public/ws.bundle.js"},
+{"thiss": __dirname+"/dist/index.pug","target":__dirname+"/../views/index.pug"}
+];
+function move(thiss, target)
+fs.readFile(thiss, (err,data) =>{
+if(err){
+console.log("Move Failed");
+}
+fs.writeFile(target, data, (err) =>{
+if(err){
+console.log("Move Failed");
+}else{
+console.log("Move Complete file "+thiss+" to "+target);
+}
+});});
